874cf527265991957877d7c016df15c249def1f0
sandymosaad <sandymosaad24@gmail.com>
2026-01-14 11:37:38 +0200

    Set up main routes for the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AllTickets from "./pages/AllTickets/AllTickets";
+import AddTicket from "./pages/AddTicket/AddTicket";
+import EdiTicket from "./pages/EditTicket/EditTicket";
+
+const router = createBrowserRouter([
+  { path: "/", element: <AllTickets /> },
+  { path: "/add", element: <AddTicket /> },
+  { path: "/edit/:id", element: <EdiTicket /> },
+  { path: "/ticket/:id", element: <EdiTicket /> },
+]);
 
 export default function App() {
-  return <>
-  <h1>hi</h1>
-  </>
+  return <RouterProvider router={router} />;
 }
---
+++ src/pages/EditTicket/EditTicket.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EdiTicket() {
+  return (
+    <div>EdiTicket</div>
+  )
+}
