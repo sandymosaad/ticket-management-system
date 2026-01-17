0a90a475ef4ba987c6ac238709f0eca78771454b
Sandy <sandymosaad24@gmail.com>
2026-01-17 15:38:37 +0200

    Implement sign-in functionality and display error if user data is incorrect

--- ticket-management-system-next/app/login/page.jsx
+++ ticket-management-system-next/app/login/page.jsx
@@ -6,10 +6,31 @@ import  Link  from 'next/link';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { getUsers} from '../lib/data-service';
+import { supabase } from "../lib/supabaseClient";
+import { useRouter } from 'next/navigation';
+import {useState} from "react"  
+
 export default function SignIn() {
+    const router = useRouter();
+    const [errorMessage, setErrorMessage] = useState("")
+
     console.log(getUsers)
-    function submitSignIm(values) {
+    async function submitSignIm(values) {
         console.log(values)
+    
+        let { data, error } = await supabase.auth.signInWithPassword({
+        email: values.email,
+        password: values.password,
+        });
+
+        if(error){
+            console.error("Error signing in:", error);
+            setErrorMessage(error.message);
+            return;
+        }
+        console.log("User signed in successfully:", data);
+        router.push('/')
+
     }
 
     const formik =useFormik({
@@ -19,8 +40,8 @@ export default function SignIn() {
         },
         onSubmit:submitSignIm,
     })
-  return <>
-  <div className={style.form}>
+return <>
+<div className={style.form}>
     <div className={style.formHeader}>
         <span className={style.iconSpan }>
             <FontAwesomeIcon icon={faRightToBracket} className={style.icon }/>
@@ -32,27 +53,28 @@ export default function SignIn() {
         <div className={style.inputContainer}>
             <label className={style.formLabel}>Email Address</label>
             <input
-             type='email'
-             name='email'
-             placeholder='you@example.com'
-             className={style.formInput}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values['email']}
+                type='email'
+                name='email'
+                placeholder='you@example.com'
+                className={style.formInput}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values['email']}
             />
         </div>
         <div className={style.inputContainer}>
             <label className={style.formLabel}>Password</label>
             <input
-             type='password'
-             name='password'
-            placeholder='*********'
-            className={style.formInput}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values['password']}
+                type='password'
+                name='password'
+                placeholder='*********'
+                className={style.formInput}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values['password']}
             />
         </div>
+        {errorMessage && <p className={style.error}>{errorMessage}</p>}
         <div className={style.formFotter}>
             <button type='submit' className={style.button}>
                 sign in
@@ -61,7 +83,7 @@ export default function SignIn() {
         </div>
 
     </form>
-  </div>
-  </>
+</div>
+</>
     
 }
