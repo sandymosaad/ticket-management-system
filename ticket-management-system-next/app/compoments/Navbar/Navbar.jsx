41f8147f046f2d15790cf34fb1385217f7759b79
sandymosaad <sandymosaad24@gmail.com>
2026-01-18 15:03:49 +0200

    Add cards to display ticket status and counts

--- ticket-management-system-next/app/compoments/Navbar/Navbar.jsx
+++ ticket-management-system-next/app/compoments/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
+
 import logo from "../../assets/logo.png";
 import Image from "next/image";
 import style from "./navbar.module.css";
 import  Link  from 'next/link';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+//import { getLogedInUser } from "../../lib/data-service";
 
-export default function Navbar() {
+export default async function Navbar() {
+  // const user = await getLogedInUser();
+  //   console.log(user)
   return (
     <nav className={style.nav}>
       <div className={style.brand} >
@@ -24,7 +28,8 @@ export default function Navbar() {
       <div className={style.user}>
         <div className={style.userData}> 
           <p>Admin User</p>
-           <p>email</p>
+          <p>email</p>
+           {/* <p>{user['email']}</p> */}
         </div>
         <button className={style.logoutButton}>
           <FontAwesomeIcon icon={faRightToBracket} className={style.icon }/>
--- ticket-management-system-next/app/page.js
+++ ticket-management-system-next/app/page.js
@@ -1,10 +1,53 @@
-import { getLogedInUser } from "./lib/data-service"
+import Card from "./compoments/Card/Card";
+import {
+  faTicket,
+  faFolderOpen,
+  faSpinner,
+  faCheckCircle,
+  faTimesCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   // const user = getLogedInUser();
   // console.log(user)
+
+ const cardsData = [
+  {
+    title: "Total Tickets",
+    numbersOfTickets: "4",
+    icon: faTicket,
+  },
+  {
+    title: "Open",
+    numbersOfTickets: "2",
+    icon: faFolderOpen,
+  },
+  {
+    title: "In Progress",
+    numbersOfTickets: "1",
+    icon: faSpinner,
+  },
+  {
+    title: "Resolved",
+    numbersOfTickets: "0",
+    icon: faCheckCircle,
+  },
+  {
+    title: "Closed",
+    numbersOfTickets: "1",
+    icon: faTimesCircle,
+  },
+];
   return <>
-  <h1>hello </h1>
+  <div className="cards">
+      {
+        cardsData.map((card) => (
+          <Card key={card.title} card={card} />
+        ))
+      }
+  </div>
+
+
   
   </>
 }
