0ade85f6c0d38ce5ee486cc0cff821760261174f
sandymosaad <sandymosaad24@gmail.com>
2026-01-19 11:16:21 +0200

    Add validation to Add Ticket form using Yup and Formik and display errors

--- ticket-management-system-next/app/compoments/TicketForm/TicketForm.jsx
+++ ticket-management-system-next/app/compoments/TicketForm/TicketForm.jsx
@@ -4,18 +4,28 @@ import InputForm from "../InputForm/InputForm"
 import style from "./ticketForm.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-
+import  * as Yup from "yup";
 export default function TicketForm() {
 
       function submitTicket(values){
         console.log(values)
       }
+
+      const validationSchema = Yup.object({
+          title:Yup.string()
+          .required("Title is required"),
+          description:Yup.string()
+          .required("Description is required"),
+            status:Yup.string()
+          .required("status is required"),
+      })
       const formik =useFormik({
           initialValues:{
               title:"",
               description:"",
               status:"",
           },
+          validationSchema,
           onSubmit:submitTicket,
       })
 
@@ -24,34 +34,34 @@ export default function TicketForm() {
         { label: "Description ", type: "textarea", inputName: "description", placeholder: "Enter detailed description of the ticket" },
         { label: "Status", type: "select", inputName: "status" , options:["Open", "In Progress","Resolved", "Closed"]}, 
     ]
-  return <>
-    <div className='form'>
-      <form onSubmit={formik.handleSubmit} >
-          <h1 className={style.formHeader}>
-            <span className='iconSpan'>
-                <FontAwesomeIcon icon={faPlus} className='icon'/>
-            </span>  
-             Add New Ticket
-          </h1>
-          {inputData.map((input)=>
-          
-          <InputForm 
-          key={input.inputName} 
-          input={input} 
-          formik={formik}
-           />)}
+    return <>
+      <div className='form'>
+        <form onSubmit={formik.handleSubmit} >
+            <h1 className={style.formHeader}>
+              <span className='iconSpan'>
+                  <FontAwesomeIcon icon={faPlus} className='icon'/>
+              </span>  
+              Add New Ticket
+            </h1>
+            {inputData.map((input)=>
+            
+            <InputForm 
+            key={input.inputName} 
+            input={input} 
+            formik={formik}
+            />)}
 
-          {/* {errorMessage && <p className={style.error}>{errorMessage}</p>} */}
-          <div className={style.formFotter}>
-              <button type='submit' className={`${style.button} ${'button'}`}>
-                 Add Ticket
-              </button>
-              <button  className={`${style.button} ${style.cancelButton} ${'button'}`}>
-                Cancel
-              </button>
-          </div>
+            {/* {errorMessage && <p className={style.error}>{errorMessage}</p>} */}
+            <div className={style.formFotter}>
+                <button type='submit' className={`${style.button} ${'button'}`}>
+                  Add Ticket
+                </button>
+                <button  className={`${style.button} ${style.cancelButton} ${'button'}`}>
+                  Cancel
+                </button>
+            </div>
 
-      </form>
-    </div>
-  </>
+        </form>
+      </div>
+    </>
 }
