eaf6a5c8d2a9f5933d7e3865f585c484d06046c3
sandymosaad <sandymosaad24@gmail.com>
2026-01-27 15:38:02 +0200

    resolve PR

--- ticket-management-system-next/app/page.js
+++ ticket-management-system-next/app/page.js
@@ -1,23 +1,3 @@
-// import Card from "./components/Card/Card";
-// import cardsData from "./cardsData";
-// import AllTickets from "./components/AllTickets/AllTickets";
-// import { Suspense } from 'react';
-
-
-// export default function Home() {
-//   return <>
-//   <div className="cards">
-//       {
-//         cardsData.map((card) => (
-//           <Card key={card.title} card={card} />
-//         ))
-//       }
-//   </div>
-//     <Suspense fallback={<p>Loading...</p>}>
-//         <AllTickets/>
-//     </Suspense>
-// </>
-// }
 import Link from "next/link"
 import style from "./page.module.css"
 import {staticCardsDataForLandingPage} from "./cardsData";
