35d4df6f13c5482a0a984e927e6d678fee47184e
Sandy Mosaad <sandyzakher5@gemail.com>
2026-02-04 12:36:12 +0200

    test project

--- .next/dev/static/chunks/ticket-management-system-next_app_favicon_ico_mjs_2f9f8873._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/4cf4e_next_dist_37bd16a1._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/ticket-management-system-next_app_tickets_[id]_loading_jsx_5b5dc5a5._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/ticket-management-system-next_app_components_Spinner_spinner_module_b340f628.css"
-],
-    source: "dynamic"
-});
